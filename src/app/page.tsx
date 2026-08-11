ccffe55f31ceee16dfc7c98c31937b896bea9b31
Mohsen <mohsen242424@gmail.com>
2026-08-11 19:54:35 +0300

    Remove StatsSection completely from the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import React from 'react';
 import HeroSection from '@/components/home/HeroSection';
-import StatsSection from '@/components/home/StatsSection';
 import WhatIsNeuro from '@/components/home/WhatIsNeuro';
 import FeaturesGrid from '@/components/home/FeaturesGrid';
 import CTASection from '@/components/home/CTASection';
@@ -11,7 +10,6 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-light dark:bg-dark text-slate-900 dark:text-slate-50 selection:bg-primary/30 overflow-hidden font-inter">
       <HeroSection />
-      <StatsSection />
       <WhatIsNeuro />
       <FeaturesGrid />
       <CTASection />
